e0fbd19eebb799e0022cc77b07deb212b99f6b83
yavuzakan <103066090+yavuzakan@users.noreply.github.com>
2022-09-19 08:34:14 +0300

    Proje dosyası ekle.

---
+++ HomeRadio/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿
+namespace HomeRadio
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.LightBlue;
+            this.button2.Location = new System.Drawing.Point(84, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 40);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Pause";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.LightBlue;
+            this.button3.Location = new System.Drawing.Point(2, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 40);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Play";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.LightBlue;
+            this.button4.Location = new System.Drawing.Point(165, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 40);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Next";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(248, 57);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "HomeRadio";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ HomeRadio/Form1.cs
@@ -0,0 +1,111 @@
+﻿using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace HomeRadio
+{
+    //NAudio - Nuget  
+    public partial class Form1 : Form
+    {
+        private IWavePlayer waveOut;
+        private Mp3FileReader mp3FileReader;
+        public string[] songs = { };
+        public List<string> songlist = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            allsong();
+            PlayMp3();
+         
+
+ 
+        }
+        public void allsong()
+        {
+            /*
+            List<int> termsList = new List<int>();
+            for (int runs = 0; runs < 400; runs++)
+            {
+                termsList.Add(value);
+            }
+
+            // You can convert it back to an array if you would like to
+            int[] terms = termsList.ToArray();
+             
+             */
+            DirectoryInfo d = new DirectoryInfo(@"mp3"); //Assuming Test is your Folder
+
+            FileInfo[] Files = d.GetFiles("*.mp3"); //Getting Text files
+            string str = "";
+            
+            foreach (FileInfo file in Files)
+            {
+                songlist.Add(file.Name);
+            }
+            songs = songlist.ToArray();
+
+          
+
+        }
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+
+            
+
+
+        }
+
+        private void PlayMp3()
+        {
+      
+
+            Random random = new Random();
+            int start2 = random.Next(0, songs.Length);
+           String playsong = "mp3/" + songs[start2];
+            this.Text = songs[start2];
+
+            this.waveOut = new WaveOut(); // or new WaveOutEvent() if you are not using WinForms/WPF
+            this.mp3FileReader = new Mp3FileReader(playsong);
+            this.waveOut.Init(mp3FileReader);
+           
+            this.waveOut.Play();
+            this.waveOut.PlaybackStopped += OnPlaybackStopped;
+            
+        }
+
+        private void OnPlaybackStopped(object sender, EventArgs e)
+        {
+            this.waveOut.Dispose();
+            this.mp3FileReader.Dispose();
+            PlayMp3();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.waveOut.Pause();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.waveOut.Play();
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.waveOut.Stop();
+        }
+    }
+}
